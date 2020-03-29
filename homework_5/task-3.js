e9e694f99190283783a1e93647be46587f1e5a0c
Vita Hetman <pvp071984@gmail.com>
2020-03-29 21:01:43 +0300

    HomeWork 5 v.2

--- homework_5/task-3.js
+++ homework_5/task-3.js
@@ -1,49 +1,37 @@
 'use strict';
 
-const TodoEditor = function(todos = []) {
-    this.todos = todos;
-};
+class Storage {
+  constructor(items = []) {
+    this.items = items
+  }
 
-TodoEditor.prototype.saveTodo = function(todo){
-    this.todos.push(todo);
-};
+  getItems(items) {
+    console.table(this.items)
+  }
 
-const todoEditor = new TodoEditor();
-
-todoEditor.saveTodo({ text: 'new todo'});
-//console.log(TodoEditor);
-class Storage{
-    constructor(items = []){
-        this.items = items
-    }
-
-getItems(items){
-console.table(this.items)
-}
-
-addItem(item){
+  addItem(item) {
     this.items.push(item)
-}
+  }
 
-removeItem(item){
-  this.items.splice(this.items.indexOf(item),1);
-}
+  removeItem(item) {
+    this.items.splice(this.items.indexOf(item), 1);
+  }
 }
 
 const storage = new Storage([
-    'Нанитоиды',
-    'Пролонгер',
-    'Железные жупи',
-    'Антигравитатор',
-  ]);
-  
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор',
+]);
+
 const items = storage.getItems();
- storage.getItems();
-  //console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
-  
-  storage.addItem('Дроид');
-  storage.getItems(); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
-  
-    storage.removeItem('Пролонгер');
-    storage.getItems();
-  console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
+storage.getItems();
+//console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+storage.addItem('Дроид');
+storage.getItems(); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+storage.removeItem('Пролонгер');
+storage.getItems();
+console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
